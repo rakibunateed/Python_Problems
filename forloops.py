473639fae35b9f9948adfbbccf7de12dd5f4d31b
rakibunateed <rakibunateed@gmail.com>
2025-07-23 23:14:51 +0600

    few

---
+++ forloops.py
@@ -0,0 +1,7 @@
+num = int(input())
+for x in range(1,11):
+    print(x)
+
+num = (input())
+for x in num:
+    print(x)
---
+++ whileLoops.py
@@ -0,0 +1,15 @@
+# name = input("Enter your name: ")
+#
+# while name == "":
+#     print(f"This is the incorrect username {name}.")
+#     name = input("Please reenter your name: ")
+#
+# print(f"Welcome {name}.")
+
+num = int(input("Enter an number between 0-10: "))
+
+while num < 1 or num > 10:
+    print(f"{num} is an incorrect number.")
+    num = int(input("Enter another number between 0-10: "))
+
+print(f"{num} is the correct number.")
