142fe440a37f69e58026ca98f94e6b0aad474151
rakibunateed <rakibunateed@gmail.com>
2025-08-07 23:49:15 +0600

    nothing

--- sgs.py
+++ sgs.py
@@ -1,30 +1,11 @@
-class Animal:
-    def __init__(self,name):
-        self.name = name
-    def eat(self):
-        print(f"{self.name} is eating....")
-    def sleep(self):
-        print(f"{self.name} is sleeping...")
-class prey(Animal):
-    def flee(self):
-        print(f"{self.name} is fleeing....")
-class predator(Animal):
-    def hunt(self):
-        print(f"{self.name} is hunting....")
-class Rabbit(prey):
-    pass
-class Hwak(predator):
-    pass
-class Fish(prey, predator):
+class Shape:
     pass
 
-rabbit = Rabbit("Fardin")
-hawk = Hwak("Amit")
-fish = Fish("Atied")
+class Circle:
+    pass
 
-rabbit.flee()
-rabbit.eat()
-rabbit.sleep()
+class Square:
+    pass
 
-
-
+class Triangle:
+    pass
